c02791a2d7a1f81d2594d9f4f2515dc582a9921d
Nelson Musto <mustonelson@gmail.com>
2024-04-18 10:35:30 -0300

    Structure - Club Entity

---
+++ src/main/java/com/football/entities/Club.java
@@ -0,0 +1,28 @@
+package com.football.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Coach coach;
+    private List<Player> players;
+    private List<Competition> competitions;
+    private Association association;
+
+}
