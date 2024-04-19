9e50289366b9b48f779eacb074cb1e003aca8de9
Nelson Musto <mustonelson@gmail.com>
2024-04-19 09:02:14 -0300

    Structure: Coach Entity

--- src/main/java/com/football/entities/Coach.java
+++ src/main/java/com/football/entities/Coach.java
@@ -1,7 +1,23 @@
 package com.football.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Coach {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "last_name")
+    private String last_name;
+    private String nationality;
+    private Integer age;
 }
