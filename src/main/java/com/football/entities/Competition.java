997d177a3971ace77a67513e541783c80b73dd17
Nelson Musto <mustonelson@gmail.com>
2024-04-19 08:59:06 -0300

    Structure: Competition Entity

--- src/main/java/com/football/entities/Competition.java
+++ src/main/java/com/football/entities/Competition.java
@@ -1,7 +1,32 @@
 package com.football.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Competition {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "quantity_price")
+    private Integer quantityPrice;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
 }
