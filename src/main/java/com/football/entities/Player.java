a1d3bb326cfbfea9a8a2c110eed8f73fda1ceba4
Nelson Musto <mustonelson@gmail.com>
2024-04-19 08:48:05 -0300

    Structure: Player Entity

--- src/main/java/com/football/entities/Player.java
+++ src/main/java/com/football/entities/Player.java
@@ -1,7 +1,27 @@
 package com.football.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Columns;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private String nationality;
+    private Integer age;
+    private String position;
+    private Integer number;
+
 }
